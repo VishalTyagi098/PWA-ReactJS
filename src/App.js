4f47a9f385c0a55fd13cae7da459edc7213daa32
Vishal <vishaltyagi.dev098@gmail.com>
2023-06-29 12:35:15 +0530

    fetched data from api

--- src/App.js
+++ src/App.js
@@ -1,23 +1,23 @@
-import logo from './logo.svg';
-import './App.css';
+import React,{useState,useEffect} from "react";
+import { getAllChats } from "./api/chats";
 
 function App() {
+  const [chatData, setChatData] = useState([]);
+
+  useEffect(() => {
+    async function fetchData(){
+      const data=await getAllChats();
+      setChatData(data?.chats);
+    }
+    fetchData();
+  }, [])
+
+  
+  console.log(chatData);
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      app
     </div>
   );
 }
